63e0e08b1e1db020f7cd23d8f8c46de0eba5ed27
Atishay Jain <73696719+Atijian@users.noreply.github.com>
2024-12-29 14:10:22 +0530

    variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,7 +11,7 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner] # Bitnami
 }
 
 data "aws_vpc" "default"{
@@ -22,10 +22,10 @@ module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
-  cidr = "10.0.0.0/16"
+  cidr = "${var.Environment.network_prefix}).0.0/16"
 
   azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.Environment.network_prefix}.0.101.0/24", "${var.Environment.network_prefix}.0.102.0/24", "${var.Environment.network_prefix}.0.103.0/24"]
 
   tags = {
     Terraform = "true"
@@ -72,7 +72,7 @@ module "alb" {
   security_group_egress_rules = {
     all = {
       ip_protocol = "-1"
-      cidr_ipv4   = "10.0.0.0/16"
+      cidr_ipv4   = "${var.Environment.network_prefix}.0.0.0/16"
     }
   }
 
--- variables.tf
+++ variables.tf
@@ -2,3 +2,40 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description = "name filter and owner of ami"
+
+  type = object({
+    name = string
+    owner = string
+  })
+
+  default ={
+    name = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = ""979382823631""
+  }
+}
+
+variable "Environment" {
+  description = "Development Environment"
+  type = object({
+    name = string
+    network_prefix = string
+  })
+
+  default = {
+    name= "dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "min_size" {
+  description = "minimum size of autoscale group"
+  default = 1
+}
+
+variable "max_size" {
+  description = "maximum size of autoscale group"
+  default = 2
+}
